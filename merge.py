3a13407d9690eeae9a92ed0983fab75a2176e226
swolepenguin <ashtonm223@gmail.com>
2021-01-12 09:04:01 -0500

    first attempt

---
+++ merge.py
@@ -0,0 +1,33 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    midpoint = int(len(array) / 2)
+
+    left, right = merge_sort(array[:midpoint]), merge_sort(array[midpoint:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    left_pointer = right_pointer = 0
+
+    while left_pointer < len(left) and right_pointer < len(right):
+
+        if left[left_pointer] < right[right_pointer]:
+
+            result.append(left[left_pointer])
+            left_pointer += 1
+
+        else: 
+
+            result.append(right[right_pointer])
+            right_pointer += 1
+
+    result.extend(left[left_pointer:])
+    result.extend(right[right_pointer:])
+
+    return result
+
+
+print(merge_sort([6,3,8,7,2,1,4,5]))
